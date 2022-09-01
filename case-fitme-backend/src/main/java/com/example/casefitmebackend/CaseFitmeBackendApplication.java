12d52284841c22017260b901637e1f2e2e339763
suay-selay <70511142+suay-selay@users.noreply.github.com>
2022-09-01 10:40:24 +0200

    initial commit

---
+++ case-fitme-backend/src/main/java/com/example/casefitmebackend/CaseFitmeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.casefitmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseFitmeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseFitmeBackendApplication.class, args);
+	}
+
+}
---
+++ case-fitme-backend/src/test/java/com/example/casefitmebackend/CaseFitmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casefitmebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseFitmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
