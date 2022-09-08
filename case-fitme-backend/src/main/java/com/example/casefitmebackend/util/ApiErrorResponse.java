9815423d4ca6cf722cc1724f3e4bf06bd9a02a99
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-09-08 13:17:27 +0200

    chore(apiErrorResponse): expanded apiErrorResponse

--- case-fitme-backend/src/main/java/com/example/casefitmebackend/util/ApiErrorResponse.java
+++ case-fitme-backend/src/main/java/com/example/casefitmebackend/util/ApiErrorResponse.java
@@ -1,4 +1,58 @@
 package com.example.casefitmebackend.util;
 
 public class ApiErrorResponse {
+    private String timeStamp;
+    private Integer status;
+    private String error;
+    private String trace;
+    private String message;
+    private String path;
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getTrace() {
+        return trace;
+    }
+
+    public void setTrace(String trace) {
+        this.trace = trace;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
